34732b6b97d9f386b211b784669663d8dde4d43b
Jakob Draeger <jakob.draeger@web.de>
2017-05-09 21:01:40 +0200

    "Added testcases for org.jis.Generator#rotateImage and a test picture 'picture'."

--- iMage/jmjrst.main/src/test/java/jmjrst/main/RotateImageTest.java
+++ iMage/jmjrst.main/src/test/java/jmjrst/main/RotateImageTest.java
@@ -1,5 +1,63 @@
 package jmjrst.main;
 
+import static org.junit.Assert.*;
+
+import java.awt.ImageCapabilities;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.jis.generator.Generator;
+import org.junit.Test;
+
 public class RotateImageTest {
+    
+  Generator generator;
+  BufferedImage image;
+  
+  /**
+   *  .
+   */
+  public void setUp() {
+    this.generator = new Generator(null, 0);
+    try {
+      this.image = ImageIO.read(new File("src/test/resources/picture.jpg"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+  
+  private boolean areImagesEqual(BufferedImage image1, BufferedImage image2) {
+    if (image1.getWidth() == image2.getWidth() && image1.getHeight() == image2.getHeight()) {
+      for (int i = 0; i < image1.getWidth(); i++) {
+        for (int j = 0; j < image1.getHeight(); j++) {
+          if (image1.getRGB(i, j) != image2.getRGB(i, j)) {
+            return false;
+          }
+        }
+      }
+    } else {
+      return false;
+    }
+    return true;
+  }
+  
+  @Test
+  public void rotateImageTest1() {
+    this.setUp();
+    BufferedImage sameImage = this.generator.rotateImage(image, 0.0);
+    assertTrue(this.areImagesEqual(this.image, sameImage));
+  }
+  
+  @Test
+  public void rotateImageTest2() {
+    this.setUp();
+    BufferedImage nullImage = this.generator.rotateImage(null, 0.0);
+    assertNull(nullImage);
+  }
+
+
 
 }
