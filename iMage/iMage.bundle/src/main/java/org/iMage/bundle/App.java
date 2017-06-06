edadab4062e50fbe41d91d7d9d7ddecb22a398f6
Jakob Draeger <jakob.draeger@web.de>
2017-06-06 16:40:17 +0200

    "Added the plugin iMage.bundle."

---
+++ iMage/iMage.bundle/src/main/java/org/iMage/bundle/App.java
@@ -0,0 +1,13 @@
+package org.iMage.bundle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      org.jis.Main.main(new String[0]);
+    }
+}
